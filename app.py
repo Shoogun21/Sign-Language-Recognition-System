3f7d973ebcfc8011addd170a458ee879fc3994da
John Yamashita <137283543+Shoogun21@users.noreply.github.com>
2026-02-22 18:34:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app.py
 Description : This script is used to run and test the trained Sign Language Recognition system.
               It imports the main application from the `slr` module and executes it.
 
-Author      : Sam <sam@codingsamrat.com>
+Author      : John Paul Yamashita <johnyamashita211@gmail.com>
 Created     : May 26, 2023
 """
 
